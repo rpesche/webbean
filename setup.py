c5f2e7076394be2966b48e1c48fe0b5980e9c626
Romain PESCHE <romain.pesche@sewan.fr>
2017-12-02 13:23:28 +0100

    Ajout setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+setup(
+    name="Webbean",
+    version="0.0.1",
+    author="Pesche Romain",
+    author_email="rpesche@pesche.fr",
+    description=("An application to get transactions diff"
+                 "between accounting file and bank"),
+    license="GNU AGPL 3",
+    keywords="accounting weboob synchronization",
+    url="http://packages.python.org/webbean",
+    packages=['webbean'],
+    long_description=open('README').read(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Topic :: Office/Business :: Financial :: Accounting",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: BSD License",
+    ],
+
+    entry_points={
+            'console_scripts': [
+                'webbean = bank:main',
+            ],
+        },
+)
