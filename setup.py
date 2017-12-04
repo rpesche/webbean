ee4f7ecc398c8af4fc07e6fee1cb5d41e90eb7b9
Romain PESCHE <romain.pesche@sewan.fr>
2017-12-04 18:31:54 +0100

    Ajout packages dans setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description=("An application to get transactions diff"
                  "between accounting file and bank"),
     license="GNU AGPL 3",
+    packages=find_packages(),
     include_package_data=True,
     keywords="accounting weboob synchronization",
     url="http://packages.python.org/webbean",
